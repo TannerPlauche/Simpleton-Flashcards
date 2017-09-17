42318d1415b6ee1dd0571555e9cc6db1bf328068
Tanner Plauché <tannerplauche@Tanners-MacBook-Pro.local>
2017-09-16 22:46:33 -0500

    Fix 'list' naming conflict, add card & collection routers

---
+++ backend/routes/cardRoutes.js
@@ -0,0 +1,5 @@
+var express = require("express");
+var cardRouter = express.Router();
+
+
+module.exports = cardRouter;
---
+++ backend/routes/collectionRoutes.js
@@ -0,0 +1,5 @@
+var express = require("express");
+var collectionRouter = express.Router();
+
+
+module.exports = collectionRouter;
